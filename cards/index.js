a82f862af294fd5777a93c4c10b132536dba69a6
Diyumana Bhardwaj <bhardwajdiyumana@gmail.com>
2025-04-24 22:15:44 +0530

    Add files via upload

---
+++ cards/index.js
@@ -0,0 +1,8 @@
+let n = [12,77,44,34]
+let m = n.toString()
+// console.log(n.splice(3,3,23,24))
+// console.log(n.reverse());
+// console.log(n);
+n.forEach((hello) =>{
+    console.log(hello);
+})
